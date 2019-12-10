31b80d3ddf605b63ed840de24d474ea9d25f02d8
ianjum <ianjum@ncsu.edu>
2019-12-09 19:32:52 -0500

    update

--- slim_example/nets/cifarnet.py
+++ slim_example/nets/cifarnet.py
@@ -66,14 +66,14 @@ def cifarnet(images, num_classes=10,
   with tf.compat.v1.variable_scope(scope, 'CifarNet', [images, num_classes]):
     device = '/gpu:0'
     with tf.device(device):
-#       net = slim.conv2d(images, 64, [5, 5], scope='conv1')
-      net = ulayer.conv2d(images, 64, [5, 5], scope='conv1', quantized=True, quantization_params=quant_params[0])
+      net = slim.conv2d(images, 64, [5, 5], scope='conv1')
+#      net = ulayer.conv2d(images, 64, [5, 5], scope='conv1', quantized=True, quantization_params=quant_params[0])
       end_points['conv1'] = net
       net = slim.max_pool2d(net, [2, 2], 2, scope='pool1')
       end_points['pool1'] = net
       net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm1')
       net = slim.conv2d(net, 64, [5, 5], scope='conv2')
-#       net = ulayer.conv2d(net, 64, [5, 5], scope='conv2', quantized=True, quantization_params=quant_params[1])
+#     net = ulayer.conv2d(net, 64, [5, 5], scope='conv2', quantized=True, quantization_params=quant_params[1])
       end_points['conv2'] = net
       net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm2')
       net = slim.max_pool2d(net, [2, 2], 2, scope='pool2')
